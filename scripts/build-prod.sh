9beebdff6fc7cd7450c1a1afb73946649ccfcc9e
Elliot Winkler <elliot.winkler@gmail.com>
2024-05-06 22:45:29 -0600

    Add GitHub workflow to deploy site to Vercel
    This workflow will be kicked off automatically when the "writings" repo
    is pushed to.

--- scripts/build-prod.sh
+++ scripts/build-prod.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-git clone https://github.com/mcmire/personal-content--writings /tmp/personal-content--writings &&
-  ln -s /tmp/personal-content--writings/posts src/content/posts &&
+if [[ -z "$WRITINGS_REPO_URL" ]]; then
+  echo "WRITINGS_REPO_URL is required."
+  exit 1
+fi
+
+if [[ -z "$WRITINGS_REPO_REF" ]]; then
+  echo "WRITINGS_REPO_REF is required."
+  exit 1
+fi
+
+git clone --branch "$WRITINGS_REPO_REF" "$WRITINGS_REPO_URL" /tmp/writings &&
+  ln -s /tmp/writings/posts src/content/posts &&
   astro build
